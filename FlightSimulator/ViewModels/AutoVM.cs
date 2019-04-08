2612ed97e4de97c03d16035e8db0a04997aad735
adishani15 <adishani15@gmail.com>
2019-04-08 15:59:57 +0300

    adi changes

---
+++ FlightSimulator/ViewModels/AutoVM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using FlightSimulator.Model;
+
+namespace FlightSimulator.ViewModels
+{
+    class AutoVM
+    {
+        private ICommand _connectCommand;
+        public ICommand ConnectCommand
+        {
+            get
+            {
+                Console.WriteLine("adi");
+                return _connectCommand ?? (_connectCommand =
+                new CommandHandler(() => OnClick()));
+            }
+            set
+            {
+                Console.WriteLine("adi");
+            }
+        }
+        private void OnClick()
+        {
+            /*m_flightManager.Connect();
+            IsDisconnected = false; // Setting that the server is
+            connected*/
+        }
+    }
+}
